ab0704a80538ecf7929f7ff4f1ca715cc54a5a42
Jim Deane <jim@deane.com>
2023-05-06 18:31:10 +0100

    d
    D

--- scripts.js
+++ scripts.js
@@ -1,6 +1,9 @@
+window.onload = function() {
+	
 document.getElementById("myBtn").addEventListener("click", displayDate);
 
 function displayDate() {
   document.getElementById("demo").innerHTML = Date();
 }
-}
+
+};
